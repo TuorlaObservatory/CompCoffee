666614bf8b2250b47fede47609143a545c089c23
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-05-16 10:52:53 +0300

    Code examples cleaned

--- r/intro/examples/plain.R
+++ r/intro/examples/plain.R
@@ -3,16 +3,19 @@ library(rjags)  # Loads library for R <--> JAGS interoperability
 
 JAGSDemo <- function(){
     
-    # Number of iterations
+    # Number of iterations & chains
     n_iter <- 1e4
     n_ch <- 3
     
-    # Genreating data
+    # Genreating 100 data points
     n <- 100L
+    # Linear model function
     modelF <- function(z) 0.23 * z - 64 
+    # Simulated data with random noise
     data <- data.frame(x = 1:n)
-    data$y = modelF(data$x + abs(data$x - mean(data$x))*rnorm(nrow(data), 0, 1))
+    data$y = modelF(data$x) + rnorm(nrow(data), 0, 1) * 3
 
+    # JAGS model as a string; Can also be loaded from file
     modelStr <- 
         "model{
             a ~ dunif(-1, 1)
@@ -25,48 +28,70 @@ JAGSDemo <- function(){
             }
         }
         "
-
+    # JAGS input data
     input <- list(
         x = data$x,
         obs = data$y
     )
 
+    # Connection simulates file access
     con <- textConnection(modelStr)
+    # Model is created and burned `n_iter` iterations
     mdl <- jags.model(con, input, n.chain = n_ch, n.adapt = n_iter)
+    # Disposing resources
     close(con)
 
+    # Updates model, spinning for additional n_iter per each chain
     update(mdl, n_iter)
 
+    # Names of variables to sample
     varNames <- c("a", "b", "scat")
 
+    # Samples n_iter measurements per each variable per each chain
     smpl <- coda.samples(mdl, varNames, n_iter)
 
+    # Debug plot of posteriors
+    plot(smpl)
+
+    # Combine chains into one data frame
     combResult <- as.data.frame(do.call("rbind", smpl))
     names(combResult) <- varNames
 
+    # Calculate statistics per each variable
     statList <- lapply(combResult, function(x) 
         return(c("M" = mean(x), "S" = sd(x), quantile(x, prob = c(0.16, 0.84)))))
 
     stat <- as.data.frame(do.call(rbind, statList))
 
+    # Fitted function, p is parameter obtained in simulations
     fittedF <- function(z, p) p[1] * z + p[2]
+    # Reconstructing our data
     data$y_fit <- fittedF(data$x, stat$M)
+    # Estimated intrinsic scatter
     scat <- stat["scat", "M"]
 
+    # Opens new `device` (window)
+    dev.new()
+    # 'Empty' plot to define plot areas
     plot(NA,
         xlim = range(data$x), ylim = range(data$y),
         xlab = "Argument x", ylab = "Observation y")
     
+    # Highlighting area of intrinsic scatter around the model
     polygon(c(data$x, rev(data$x)), 
         c(data$y_fit - scat, rev(data$y_fit + scat)), 
         col = "#40FF40A0", lty = 0)
     
+    # Source data
     points(data$x, data$y, pch = 19, cex = 1.25)
 
+    # Model function
     lines(data$x, modelF(data$x), lty = 2, col = "red", lwd = 2)
 
+    # Fitted function
     lines(data$x, data$y_fit, lty = 1, col = "blue", lwd = 2)
 
+    # Prints out statistical results
     print(stat)
 }
 
--- r/intro/examples/tidy.R
+++ r/intro/examples/tidy.R
@@ -1,18 +1,21 @@
-library(rjags)
-library(tidyverse)
+library(rjags)  # Loads library for R <--> JAGS interoperability
+library(tidyverse) # Library for data manipulation & plotting
 
 JAGSDemoTidy <- function() {
     
-    # Number of iterations and chains
+    # Number of iterations & chains
     n_iter <- 1e4
     n_ch <- 3L
     
-    #Generating data
+    # Genreating 100 data points
     n <- 100L
+    # Linear model function
     modelF <- function(z) 0.23 * z - 64 
+    # Simulated data with random noise
     data <- tibble(x = 1.0 * (1:n)) %>%
-        mutate(y = modelF(x + abs(x - mean(x)) * ( rnorm(nrow(.), 0, 1)))) 
+        mutate(y = modelF(x)  + rnorm(nrow(.), 0, 1) * 3)
     
+    # JAGS model as a string; Can also be loaded from file
     modelStr <- 
         "model{
             a ~ dunif(-1, 1)
@@ -25,27 +28,38 @@ JAGSDemoTidy <- function() {
             }
         }
         "
-
+    # JAGS input data
     input <- list(
         x = data$x,
         obs = data$y
     )
 
+    # Connection simulates file access
     con <- textConnection(modelStr)
+    # Model is created and burned `n_iter` iterations
     mdl <- jags.model(con, input, n.chain = n_ch, n.adapt = n_iter)
+    # Disposing resources
     close(con)
 
+    # Updates model, spinning for additional n_iter per each chain
     update(mdl, n_iter)
 
+    # Names of variables to sample
     varNames <- c("a", "b", "scat")
     
+    # Samples n_iter measurements per each variable per each chain
     smpl <- coda.samples(mdl, varNames, n_iter)
     
+    # Debug plot of posteriors
+    plot(smpl)
+
+    # Combine chains into one data frame
     combResult <- smpl %>%
         do.call("rbind", .) %>%
         as.tibble %>%
         setNames(., nm = varNames) 
 
+    # Calculate statistics per each variable
     stat <- combResult %>% 
         sapply(FUN = function(x)
             return(c("M" = mean(x), "S" = sd(x), 
@@ -55,27 +69,39 @@ JAGSDemoTidy <- function() {
         mutate(Var = varNames) %>%
         select(Var, everything())
 
+    # Fitted function, p is parameter obtained in simulations
     fittedF <- function(z, p) p[1] * z + p[2]
+    # Reconstructing our data
     data <- data %>%
         mutate(y_fit = fittedF(x, stat %>% pull(M))) %>%
         mutate(y_mdl = modelF(x))
-
+    # Estimated intrinsic scatter
     scat <- stat %>% filter(Var == "scat") %>% pull(M)
 
-    plt <- data %>% ggplot(aes(x, y)) +
-        theme_bw() +
+    # Constructing a ggplot object
+    plt <- data %>% ggplot(aes(x, y)) + # Send data to ggplot
+        theme_bw() +                    # Apply empty theme
+        # Area of intrinsic scatter, bottom most layer
         geom_ribbon(aes(x, ymin = y_fit - scat, ymax = y_fit + scat),
-            fill = "#40FF40", alpha = 0.63) +
-        geom_point(size = 2) + 
+            fill = "#40FF40", alpha = 0.63) + 
+        # Source data, points
+        geom_point(size = 2) +
+        # Model function, line 
         geom_line(aes(x, y_mdl), 
             colour = "red", size = 1.25, linetype = 2) +
+        # Fitted function, line
         geom_line(aes(x, y_fit), 
             colour = "blue", size = 1.25) + 
+        # Axis labels
         xlab("Argument x") + 
         ylab("Observation y")
        
-
+    # Opens new `device` (window)
+    dev.new()
+    # Plots created plot on the new device
     plt %>% print
+
+    # Prints out statistical results
     stat %>% print
 }
 
